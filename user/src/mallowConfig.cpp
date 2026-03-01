79793ba8f6a68198bec907afd9305f13045b1667
jacksonnx <jacksonh.dot@gmail.com>
2026-02-28 16:23:53 -0800

    Update mallowConfig.cpp

--- user/src/mallowConfig.cpp
+++ user/src/mallowConfig.cpp
@@ -3,14 +3,11 @@
 #include <nn/fs/fs_directories.h>
 #include <nn/fs/fs_types.h>
 
-#include "ModOptions.h"
-
 namespace mallow::config {
     const char* path = "sd:/atmosphere/contents/0100000000010000/mallow.json";
     const char* pathEmu = "sd:/mallow.json";
     const char* defaultConfig = R"(
 {
-    "myModOption": true,
     "logger": {
         "enable": false,
         "reconnect": false,
@@ -23,10 +20,6 @@ namespace mallow::config {
     Allocator* getAllocator() {
         static DefaultAllocator allocator = {};
         return &allocator;
-    }
-    ConfigBase* getConfig() {
-        static ModOptions modConfig = {};
-        return &modConfig;
     }
     bool isEmu(){
         nn::fs::DirectoryEntryType type;
